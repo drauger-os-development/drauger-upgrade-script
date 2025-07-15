caeb94bb41e6ad80551b1d124e55a3616fc7c574
Thomas <contact@draugeros.org>
2025-07-14 23:10:28 -0400

    build fixes for CI/CD, allow hiding upgrade scripts before they are ready

--- build.sh
+++ build.sh
@@ -53,9 +53,12 @@ if [ -d opt ]; then
 fi
 
 cp -R DEBIAN ../"$FOLDER"/DEBIAN
+base=$PWD
 cd ..
 #DELETE STUFF HERE
 #build the shit
 #build config
 dpkg-deb --build "$FOLDER"
 rm -rf "$FOLDER"
+mkdir -p "$base/build"
+cp -v "$FOLDER.deb" "$base/build"
